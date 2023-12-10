84449a769a87625f8680fb7f7b0cbd0f96c1e526
Trenton200913 <111209161+Trenton200913@users.noreply.github.com>
2023-12-10 13:30:48 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,41 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.gameOver(false)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
+    game.gameOver(true)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    for (let index = 0; index < 4; index++) {
+        mySprite.vy += -40
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level7`)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vx = -200
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile0`)
+    tiles.setCurrentTilemap(tilemap`level10`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vx = 200
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    mySprite.vy += -90
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile0`)
+    tiles.setCurrentTilemap(tilemap`level13`)
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(8)
+mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level`)
+scene.cameraFollowSprite(mySprite)
+tiles.placeOnRandomTile(mySprite, assets.tile`myTile0`)
+mySprite.setBounceOnWall(true)
+forever(function () {
+    mySprite.vy = 100
+})
